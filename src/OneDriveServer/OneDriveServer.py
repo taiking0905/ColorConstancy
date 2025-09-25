db12531f0634bea41dcc086025ae6fdcacd00524
taiking0905 <taiki.noguchi0905@gmail.com>
2025-09-25 12:14:24 +0900

    dekitenai

--- src/OneDriveServer/OneDriveServer.py
+++ src/OneDriveServer/OneDriveServer.py
@@ -15,7 +15,6 @@ class DNGHandler(FileSystemEventHandler):
     def process_dng(self, dng_path):
         try:
             with rawpy.imread(dng_path) as raw:
-                # デモザイク後のRGBを16bitで取得（ホワイトバランスなし・ガンマ補正なし）
                 rgb = raw.postprocess(
                     use_camera_wb=False,
                     no_auto_bright=True,
@@ -25,17 +24,14 @@ class DNGHandler(FileSystemEventHandler):
                     demosaic_algorithm=rawpy.DemosaicAlgorithm.AHD,
                     output_color=rawpy.ColorSpace.raw
                 )
-
                 filename = os.path.splitext(os.path.basename(dng_path))[0] + ".png"
                 save_path = os.path.join(OneDrive_PNG_PATH, filename)
-                cv2.imwrite(save_path, cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR))
-                # print(f"\nファイル: {dng_path}")
-                # print("  AsShotNeutral:", as_shot_neutral)
-                # print("  WB multipliers:", wb_multipliers)
-                # print("  white_level:", white_level, "black_level:", black_level)
-                # print("  RAW最小値:", min_val)
-                print(f"保存しました: {save_path}")
-
+                success = cv2.imwrite(save_path, cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR))
+                if success:
+                    print(f"保存成功: {save_path}")
+                else:
+                    print(f"保存失敗: {save_path} （フォルダ存在しない可能性あり）")
+                
         except Exception as e:
             print(f"処理中にエラーが発生しました ({dng_path}): {e}")
 
@@ -44,12 +40,8 @@ class DNGHandler(FileSystemEventHandler):
             print(f"新規ファイル検知: {event.src_path}")
             self.process_dng(event.src_path)
 
-    def on_modified(self, event):
-        if event.src_path.lower().endswith(".dng"):
-            print(f"更新ファイル検知: {event.src_path}")
-            self.process_dng(event.src_path)
-
 if __name__ == "__main__":
+    print({OneDrive_PNG_PATH})
     event_handler = DNGHandler()
     observer = Observer()
     observer.schedule(event_handler, OneDrive_DATA_PATH, recursive=False)
